b3101196891625123bbad5a80b24f6295eaea5cb
skyDe <sky_de1991@outlook.com>
2024-04-24 08:46:59 -0800

    back btn added to project with functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
 
 import {
   GsapFrom,
@@ -12,20 +12,26 @@ import {
 } from "./pages";
 
 const App = () => {
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
+  const handleBack = () => navigate(-1);
   return (
-    <div className="bg-black min-h-screen w-full">
-      <Router>
-        <Routes>
-          <Route path="/gsapto" element={<GsapTo />} />
-          <Route path="/gsapfrom" element={<GsapFrom />} />
-          <Route path="/gsapfromto" element={<GsapFromTo />} />
-          <Route path="/gsaptimeline" element={<GsapTimeline />} />
-          <Route path="/gsapstagger" element={<GsapStagger />} />
-          <Route path="/gsapscrolltrigger" element={<GsapScrollTrigger />} />
-          <Route path="/gsaptext" element={<GsapText />} />
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Router>
+    <div className="bg-black min-h-screen w-full py-8">
+      {pathname !== "/" && (
+        <button className="m-4" onClick={handleBack}>
+          Back
+        </button>
+      )}
+      <Routes>
+        <Route path="/gsapto" element={<GsapTo />} />
+        <Route path="/gsapfrom" element={<GsapFrom />} />
+        <Route path="/gsapfromto" element={<GsapFromTo />} />
+        <Route path="/gsaptimeline" element={<GsapTimeline />} />
+        <Route path="/gsapstagger" element={<GsapStagger />} />
+        <Route path="/gsapscrolltrigger" element={<GsapScrollTrigger />} />
+        <Route path="/gsaptext" element={<GsapText />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 };
