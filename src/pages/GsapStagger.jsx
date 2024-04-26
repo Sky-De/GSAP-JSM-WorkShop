bdf0433b6e76a21f4c31589ef402c1bfead4a8df
skyDe <sky_de1991@outlook.com>
2024-04-26 08:33:20 -0800

    gsap stragger added

--- src/pages/GsapStagger.jsx
+++ src/pages/GsapStagger.jsx
@@ -1,5 +1,26 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const GsapStagger = () => {
-  // TODO: Implement the gsap.stagger() method
+  // DONE: Implement the gsap.stagger() method
+  useGSAP(() => {
+    gsap.to(".stagger-box", {
+      y: 50,
+      rotation: 360,
+      borderRadius: "100%",
+      repeat: -1,
+      yoyo: true,
+      duration: 1,
+      // stagger: 0.5,
+      stagger: {
+        amount: 1.5,
+        // grid: [1, 2],
+        // axis: "x",
+        ease: "cric.inOut",
+        from: "edges",
+      },
+    });
+  }, []);
 
   return (
     <main>
@@ -31,7 +52,7 @@ const GsapStagger = () => {
       </p>
 
       <div className="mt-20">
-        <div className="flex gap-5">
+        <div className="flex gap-x-5 flex-wrap gap-y-16">
           <div className="w-20 h-20 bg-indigo-200 rounded-lg stagger-box" />
           <div className="w-20 h-20 bg-indigo-300 rounded-lg stagger-box" />
           <div className="w-20 h-20 bg-indigo-400 rounded-lg stagger-box" />
